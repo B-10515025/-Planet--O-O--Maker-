00246c54e5323cb97d2bbc030de73ca4bf3e6dde
B-10515025 <ddt22771508@gmail.com>
2017-01-27 01:48:02 +0800

    ++

---
+++ PlanetEditor/Assets/PlanetPlugin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Planet))]
+public class PlanetPlugin : Editor {
+    [MenuItem("PlanetPlugin/CreatePlanet")]
+    static void CreatePlanet()
+    {
+        GameObject planet = new GameObject("planet");
+        planet.gameObject.AddComponent<Planet>();
+        MeshRenderer mr = planet.AddComponent<MeshRenderer>();
+        MeshFilter filter = planet.AddComponent<MeshFilter>();
+        mr.material = new Material(Shader.Find("Specular"));
+        planet.GetComponent<Planet>().make();
+    }
+}
