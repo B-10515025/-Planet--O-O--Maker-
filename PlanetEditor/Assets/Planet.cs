fc83a95aa762db7052ebc12ddf4cf6a2403d2572
B-10515025 <ddt22771508@gmail.com>
2017-02-12 08:19:51 +0800

    4+

--- PlanetEditor/Assets/Planet.cs
+++ PlanetEditor/Assets/Planet.cs
@@ -17,114 +17,198 @@ public class Planet : MonoBehaviour {
         }
         set { _radius = value; }
     }
+    double re_radius;
     [SerializeField]
-    int _resolution=30;
+    int _resolution=3;
     public int Resolution
     {
         get
         {
-            if (_resolution > 0)
+            if (_resolution <=6 )
                 return _resolution;
             else
-                return -_resolution;
+                return 6;
         }
         set { _resolution = value; }
     }
-    public void make()
+    int re_resolution;
+    Mesh mesh;
+    void Start()
     {
-        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        re_radius = Radius;
+        re_resolution = Resolution;
+        mesh = gameObject.GetComponent<MeshFilter>().mesh;
         mesh.Clear();
+        List<Vector3> vertList = new List<Vector3>();
+        Dictionary<long, int> middlePointIndexCache = new Dictionary<long, int>();
+        int recursionLevel = Resolution;
         float radius = (float)Radius;
-        // Longitude |||
-        int nbLong = 360/Resolution;
-        // Latitude ---
-        int nbLat = 360/Resolution;
-        #region Vertices
-        Vector3[] vertices = new Vector3[(nbLong + 1) * nbLat + 2];
-        float _pi = Mathf.PI;
-        float _2pi = _pi * 2f;
-
-        vertices[0] = Vector3.up * radius;
-        for (int lat = 0; lat < nbLat; lat++)
+        // create 12 vertices of a icosahedron
+        float t = (1f + Mathf.Sqrt(5f)) / 2f;
+        vertList.Add(new Vector3(-1f, t, 0f).normalized * radius);
+        vertList.Add(new Vector3(1f, t, 0f).normalized * radius);
+        vertList.Add(new Vector3(-1f, -t, 0f).normalized * radius);
+        vertList.Add(new Vector3(1f, -t, 0f).normalized * radius);
+        vertList.Add(new Vector3(0f, -1f, t).normalized * radius);
+        vertList.Add(new Vector3(0f, 1f, t).normalized * radius);
+        vertList.Add(new Vector3(0f, -1f, -t).normalized * radius);
+        vertList.Add(new Vector3(0f, 1f, -t).normalized * radius);
+        vertList.Add(new Vector3(t, 0f, -1f).normalized * radius);
+        vertList.Add(new Vector3(t, 0f, 1f).normalized * radius);
+        vertList.Add(new Vector3(-t, 0f, -1f).normalized * radius);
+        vertList.Add(new Vector3(-t, 0f, 1f).normalized * radius);
+        // create 20 triangles of the icosahedron
+        List<TriangleIndices> faces = new List<TriangleIndices>();
+        // 5 faces around point 0
+        faces.Add(new TriangleIndices(0, 11, 5));
+        faces.Add(new TriangleIndices(0, 5, 1));
+        faces.Add(new TriangleIndices(0, 1, 7));
+        faces.Add(new TriangleIndices(0, 7, 10));
+        faces.Add(new TriangleIndices(0, 10, 11));
+        // 5 adjacent faces 
+        faces.Add(new TriangleIndices(1, 5, 9));
+        faces.Add(new TriangleIndices(5, 11, 4));
+        faces.Add(new TriangleIndices(11, 10, 2));
+        faces.Add(new TriangleIndices(10, 7, 6));
+        faces.Add(new TriangleIndices(7, 1, 8));
+        // 5 faces around point 3
+        faces.Add(new TriangleIndices(3, 9, 4));
+        faces.Add(new TriangleIndices(3, 4, 2));
+        faces.Add(new TriangleIndices(3, 2, 6));
+        faces.Add(new TriangleIndices(3, 6, 8));
+        faces.Add(new TriangleIndices(3, 8, 9));
+        // 5 adjacent faces 
+        faces.Add(new TriangleIndices(4, 9, 5));
+        faces.Add(new TriangleIndices(2, 4, 11));
+        faces.Add(new TriangleIndices(6, 2, 10));
+        faces.Add(new TriangleIndices(8, 6, 7));
+        faces.Add(new TriangleIndices(9, 8, 1));
+        // refine triangles
+        for (int i = 0; i < recursionLevel; i++)
         {
-            float a1 = _pi * (float)(lat + 1) / (nbLat + 1);
-            float sin1 = Mathf.Sin(a1);
-            float cos1 = Mathf.Cos(a1);
-
-            for (int lon = 0; lon <= nbLong; lon++)
+            List<TriangleIndices> faces2 = new List<TriangleIndices>();
+            foreach (var tri in faces)
             {
-                float a2 = _2pi * (float)(lon == nbLong ? 0 : lon) / nbLong;
-                float sin2 = Mathf.Sin(a2);
-                float cos2 = Mathf.Cos(a2);
+                // replace triangle by 4 triangles
+                int a = getMiddlePoint(tri.v1, tri.v2, ref vertList, ref middlePointIndexCache, radius);
+                int b = getMiddlePoint(tri.v2, tri.v3, ref vertList, ref middlePointIndexCache, radius);
+                int c = getMiddlePoint(tri.v3, tri.v1, ref vertList, ref middlePointIndexCache, radius);
 
-                vertices[lon + lat * (nbLong + 1) + 1] = new Vector3(sin1 * cos2, cos1, sin1 * sin2) * radius;
+                faces2.Add(new TriangleIndices(tri.v1, a, c));
+                faces2.Add(new TriangleIndices(tri.v2, b, a));
+                faces2.Add(new TriangleIndices(tri.v3, c, b));
+                faces2.Add(new TriangleIndices(a, b, c));
             }
+            faces = faces2;
         }
-        vertices[vertices.Length - 1] = Vector3.up * -radius;
-        #endregion
-        #region Normales		
-        Vector3[] normales = new Vector3[vertices.Length];
-        for (int n = 0; n < vertices.Length; n++)
-            normales[n] = vertices[n].normalized;
-        #endregion
-        #region UVs
-        Vector2[] uvs = new Vector2[vertices.Length];
-        uvs[0] = Vector2.up;
-        uvs[uvs.Length - 1] = Vector2.zero;
-        for (int lat = 0; lat < nbLat; lat++)
-            for (int lon = 0; lon <= nbLong; lon++)
-                uvs[lon + lat * (nbLong + 1) + 1] = new Vector2((float)lon / nbLong, 1f - (float)(lat + 1) / (nbLat + 1));
-        #endregion
-        #region Triangles
-        int nbFaces = vertices.Length;
-        int nbTriangles = nbFaces * 2;
-        int nbIndexes = nbTriangles * 3;
-        int[] triangles = new int[nbIndexes];
-
-        //Top Cap
-        int i = 0;
-        for (int lon = 0; lon < nbLong; lon++)
+        mesh.vertices = vertList.ToArray();
+        List<int> triList = new List<int>();
+        for (int i = 0; i < faces.Count; i++)
         {
-            triangles[i++] = lon + 2;
-            triangles[i++] = lon + 1;
-            triangles[i++] = 0;
+            triList.Add(faces[i].v1);
+            triList.Add(faces[i].v2);
+            triList.Add(faces[i].v3);
         }
-
-        //Middle
-        for (int lat = 0; lat < nbLat - 1; lat++)
-        {
-            for (int lon = 0; lon < nbLong; lon++)
-            {
-                int current = lon + lat * (nbLong + 1) + 1;
-                int next = current + nbLong + 1;
-
-                triangles[i++] = current;
-                triangles[i++] = current + 1;
-                triangles[i++] = next + 1;
-
-                triangles[i++] = current;
-                triangles[i++] = next + 1;
-                triangles[i++] = next;
-            }
-        }
-
-        //Bottom Cap
-        for (int lon = 0; lon < nbLong; lon++)
-        {
-            triangles[i++] = vertices.Length - 1;
-            triangles[i++] = vertices.Length - (lon + 2) - 1;
-            triangles[i++] = vertices.Length - (lon + 1) - 1;
-        }
-        #endregion
-        mesh.vertices = vertices;
+        mesh.triangles = triList.ToArray();
+        mesh.uv = new Vector2[mesh.vertices.Length];
+        Vector3[] normales = new Vector3[vertList.Count];
+        for (int i = 0; i < normales.Length; i++)
+            normales[i] = vertList[i].normalized;
         mesh.normals = normales;
-        mesh.uv = uvs;
-        mesh.triangles = triangles;
         mesh.RecalculateBounds();
-        //MeshUtility.Optimize(mesh);
     }
-    private void Update()
+    void Update()
     {
-        make();
+        if(re_radius!=Radius||re_resolution!=Resolution)
+            Start();
+    }
+    private struct TriangleIndices
+    {
+        public int v1;
+        public int v2;
+        public int v3;
+
+        public TriangleIndices(int v1, int v2, int v3)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+        }
+    }
+    private static int getMiddlePoint(int p1, int p2, ref List<Vector3> vertices, ref Dictionary<long, int> cache, float radius)
+    {
+        // first check if we have it already
+        bool firstIsSmaller = p1 < p2;
+        long smallerIndex = firstIsSmaller ? p1 : p2;
+        long greaterIndex = firstIsSmaller ? p2 : p1;
+        long key = (smallerIndex << 32) + greaterIndex;
+
+        int ret;
+        if (cache.TryGetValue(key, out ret))
+        {
+            return ret;
+        }
+
+        // not in cache, calculate it
+        Vector3 point1 = vertices[p1];
+        Vector3 point2 = vertices[p2];
+        Vector3 middle = new Vector3
+        (
+            (point1.x + point2.x) / 2f,
+            (point1.y + point2.y) / 2f,
+            (point1.z + point2.z) / 2f
+        );
+
+        // add vertex makes sure point is on unit sphere
+        int i = vertices.Count;
+        vertices.Add(middle.normalized * radius);
+
+        // store it, return index
+        cache.Add(key, i);
+
+        return i;
+    }
+}
+[ExecuteInEditMode]
+public class Highlighter : MonoBehaviour{
+    Light highlight;
+    public void setloc()
+    {
+        if(Camera.current.transform!=null)
+        {
+            gameObject.transform.localPosition = Camera.current.transform.localPosition;
+            gameObject.transform.LookAt(GameObject.Find("planet").transform);
+        }
+    }
+    public void Awake()
+    {
+        UnityEditor.EditorApplication.update += Update;
+    }
+    void Start()
+    {
+        highlight = gameObject.AddComponent<Light>();
+        highlight.type = LightType.Spot;
+        highlight.color = Color.blue;
+        highlight.range = 1000;
+        highlight.spotAngle = 45;
+    }
+    void Update()
+    {    
+        if (Selection.activeGameObject.name == "planet")
+        {
+            setloc();
+            Mesh mesh = GameObject.Find("planet").GetComponent<MeshFilter>().mesh;
+            int i = 0;
+            Vector3[] vertices = mesh.vertices;
+            while (i < vertices.Length)
+            {
+                if (Vector3.Distance(vertices[i], gameObject.transform.position) < 50)
+                    vertices[i] = vertices[i] *1.0005f;
+                mesh.normals[i] = vertices[i].normalized;
+                i++;
+            }
+            mesh.vertices = vertices;
+            mesh.RecalculateBounds();
+        }
     }
 }
--- PlanetEditor/Assets/PlanetPlugin.cs
+++ PlanetEditor/Assets/PlanetPlugin.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(Planet))]
 public class PlanetPlugin : Editor {
     [MenuItem("PlanetPlugin/CreatePlanet")]
     static void CreatePlanet()
     {
+        GameObject.Find("Directional Light").GetComponent<Light>().intensity = 0;
+        GameObject highlighter = new GameObject("highlighter");
+        highlighter.AddComponent<Highlighter>();
         GameObject planet = new GameObject("planet");
-        planet.gameObject.AddComponent<Planet>();
+        planet.AddComponent<Planet>();
         MeshRenderer mr = planet.AddComponent<MeshRenderer>();
         MeshFilter filter = planet.AddComponent<MeshFilter>();
         mr.material = new Material(Shader.Find("Specular"));
-        planet.GetComponent<Planet>().make();
     }
 }
